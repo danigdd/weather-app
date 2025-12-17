2967e933ca1acf88cccd6045630b565abd611b6e
Daniel Genov <danigenov@protonmail.com>
2025-12-17 02:31:05 +0100

    First commit

---
+++ webpack.dev.js
@@ -0,0 +1,15 @@
+/* eslint-disable */
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common');
+
+module.exports = merge(common, {
+  mode: 'development',
+
+  devtool: 'inline-source-map',
+
+  devServer: {
+    port: 3000,
+    open: true,
+    hot: true,
+  },
+});
