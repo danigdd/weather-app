9ddd713f938b99b948277673fa5afacc3af26920
Daniel Genov <danigenov@protonmail.com>
2025-12-19 22:13:31 +0100

    feat : add first approeach to cleaning data from API fetch

--- src/apiController.js
+++ src/apiController.js
@@ -4,6 +4,13 @@ export async function getDataFromAPI(cityName) {
   const response = await fetch(
     `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${cityName}?key=${API_KEY}`
   );
-  const cityData = await response.json();
+  const cityJSON_data = await response.json();
+  const cityData = cleanDataFromAPI(cityJSON_data);
+  return cityData;
+}
+
+function cleanDataFromAPI(JSON_data) {
+  const cityData = {};
+  cityData['current_temperature'] = JSON_data.currentConditions.temp;
   return cityData;
 }
--- src/index.js
+++ src/index.js
@@ -3,7 +3,11 @@ import '../styles/general-styles.css';
 import { renderMain } from './view';
 import { getDataFromAPI } from './apiController';
 
-console.log('hi');
 renderMain();
-getDataFromAPI('washington');
-console.log('bye');
+
+async function createDataObject() {
+  const cityData = await getDataFromAPI('washington');
+  console.log(cityData['current_temperature']);
+}
+
+createDataObject();
