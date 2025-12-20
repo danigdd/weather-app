ed6d28783600ce0e97fe03d2ea88e0076faf87f7
Daniel Genov <danigenov@protonmail.com>
2025-12-20 13:43:27 +0100

    feat: add 400 error page

--- src/index.js
+++ src/index.js
@@ -2,15 +2,37 @@ import '../styles/general-styles.css';
 
 import { renderMain } from './renderMain';
 import { renderCityWeather } from './renderCityWeather';
+import { render404NotFound } from './render404NotFound';
 import { getDataFromAPI } from './apiController';
 
 async function city(city_name) {
   const cityData = await getDataFromAPI(city_name);
-  if (cityData == -1) return;
+  if (cityData == -1) {
+    console.log('what');
+    pageNotFound(city_name);
+  }
   renderCityWeather(cityData);
   const searchDOM = document.getElementById('searchButton_id');
   const textAreaDOM = document.getElementById('textArea_id');
 
+  searchDOM.addEventListener('click', () => {
+    const city_name = document.getElementById('textArea_id').value;
+    city(city_name);
+  });
+
+  textAreaDOM.addEventListener('keydown', (event) => {
+    if (event.key === 'Enter') {
+      const city_name = document.getElementById('textArea_id').value;
+      city(city_name);
+    }
+  });
+}
+
+function pageNotFound(city_name_error) {
+  console.log('entered on the function');
+  render404NotFound(city_name_error);
+  const searchDOM = document.getElementById('searchButton_id');
+  const textAreaDOM = document.getElementById('textArea_id');
   searchDOM.addEventListener('click', () => {
     const city_name = document.getElementById('textArea_id').value;
     city(city_name);
---
+++ src/render404NotFound.js
@@ -0,0 +1,37 @@
+import '../styles/general-styles.css';
+import '../styles/notFound-styles.css';
+
+const root = document.getElementById('content');
+
+export function render404NotFound(name_not_found) {
+  root.innerHTML = '';
+  root.style.animation = 'none';
+  void root.offsetWidth;
+  root.style.animation = 'fadeUp 1s ease-out';
+  console.log('we even creating it');
+
+  // TITLE
+  const titleDOM = document.createElement('div');
+  titleDOM.id = 'titleDOM_id';
+  titleDOM.textContent = `Oopss... Error 400`;
+  root.appendChild(titleDOM);
+
+  const noResultsDOM = document.createElement('div');
+  noResultsDOM.id = 'noResults_id';
+  noResultsDOM.textContent = `No results found for ${name_not_found}`;
+  root.appendChild(noResultsDOM);
+
+  // SEARCH BAR
+  const searchBarWrapperDOM = document.createElement('div');
+  searchBarWrapperDOM.id = 'searchBarWrapper_id';
+  root.appendChild(searchBarWrapperDOM);
+
+  const textAreaDOM = document.createElement('input');
+  textAreaDOM.id = 'textArea_id';
+  textAreaDOM.placeholder = 'Search for a city...';
+  searchBarWrapperDOM.appendChild(textAreaDOM);
+
+  const searchButtonDOM = document.createElement('button');
+  searchButtonDOM.id = 'searchButton_id';
+  searchBarWrapperDOM.appendChild(searchButtonDOM);
+}
